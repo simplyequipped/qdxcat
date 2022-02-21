93fa46b1090b8278c4684567a02752471c9cbb15
Howard <howard@simplyequipped.com>
2022-02-21 10:26:59 -0500

    updated init for removal of web app

--- qdx/__init__.py
+++ qdx/__init__.py
@@ -1,11 +1,1 @@
-import waitress
-
 from qdx.qdx import QDX
-from qdx import server
-#from qdx import modem
-
-def serve(**kwargs):
-    waitress.serve(server.app,**kwargs)
-
-def serve_all(port=5000, **kwargs):
-    waitress.serve(server.app, host="0.0.0.0", port=port, **kwargs)
