d84d2a9ba236f4ca905e46cd381f4d76aa5d36aa
Howard <howard@simplyequipped.com>
2022-02-20 20:26:04 -0500

    add package setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='qdx'
+    version='0.1'
+    description='QRP Labs QDX tranceiver control library'
+    author='Simply Equipped LLC'
+    author_email='info@simplyequipped.com'
+    packages=['qdx']
+    install_requires=['pyserial', 'flask', 'waitress']
+)
