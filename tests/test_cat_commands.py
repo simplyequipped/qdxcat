5d636355977cc3174fc6b1e816325d6ac301fad0
Howard <howard@simplyequipped.com>
2022-02-21 18:49:56 -0500

    added qdx symlink, improved test script

--- tests/test_cat_commands.py
+++ tests/test_cat_commands.py
@@ -1,3 +1,4 @@
+import sys
 import qdx
 
 radio = qdx.QDX()
@@ -8,39 +9,68 @@ else:
     print('QDX not found')
     exit()
 
+tx = False
+for arg in sys.argv:
+    if arg.lower() in ['--tx', '-t']:
+        tx = True
+
+total = 0
+passed = 0
+failed = 0
+skipped = 0
+
+failed_cmds = {}
+skipped_cmds = {}
+
 for cmd in radio.commands:
     print('\n' + cmd['label'] + ' (' + cmd['cmd'] + ')')
 
+    # skip tx command unless specified by user
+    if cmd['cmd'] == 'TX' and tx == False:
+        print('   skipping TX command')
+        total += 1
+        skipped += 1
+        skipped_cmds[cmd['cmd']] = cmd['label']
+        continue
+
     # get test
     try:
+        total += 1
         if cmd['get'] is not None:
             value = radio.command(cmd['cmd'])
+            if value is None:
+                raise Exception('Command not understood')
     except Exception as e:
-        print('\tget: fail')
-        print('\t\t' + e)
+        failed += 1
+        failed_cmds['get ' + cmd['label']] = cmd['cmd']
+        print('   get: fail')
+        print('      ' + str(e))
     else:
         if cmd['get'] is None:
-            print('get: None')
+            passed += 1
+            print('   get: None')
         else:
-            print('\tget: pass')
+            passed += 1
+            print('   get: pass')
 
             if cmd['map'] is None:
-                print('\t\t' + str(value))
+                print('      ' + str(value))
             else:
-                for key, val in cmd['map']:
+                for key, val in cmd['map'].items():
                     if key == str(value):
-                        print('\t\t' + val + ' (' + value + ')')
+                        print('      ' + str(val) + ' (' + str(value) + ')')
                         break
 
     # set test
     try:
+        total += 1
         if cmd['set'] is not None:
             if value is None:
                 if cmd['map'] is not None:
                     if '0' in cmd['map'].keys():
                         value = 0
                     else:
-                        for key, val in cmd['map']:
+                        for key, val in cmd['map'].items():
                             value = key
                             break
                 else:
@@ -48,11 +78,27 @@ for cmd in radio.commands:
             else:
                 radio.command(cmd['cmd'], value)
     except Exception as e:
-        print('\tset: fail')
-        print('\t\t' + e)
+        failed += 1
+        failed_cmds['set ' + cmd['label']] = cmd['cmd']
+        print('   set: fail')
+        print('      ' + str(e))
     else:
         if cmd['set'] is None:
-            print('set: None')
+            passed += 1
+            print('   set: None')
         else:
-            print('\set: pass')
+            passed += 1
+            print('   set: pass')
+
+
+print('\n\nTest Summary:')
+print('   ' + str(passed) + '/' + str(total) + ' passed')
+print('   ' + str(skipped) + '/' + str(total) + ' skipped')
+for label, cmd in skipped_cmds.items():
+    print('      ' + label + ' (' + cmd + ')')
+print('   ' + str(failed) + '/' + str(total) + ' failed')
+for label, cmd in failed_cmds.items():
+    print('      ' + label + ' (' + cmd + ')')
+print('')
+
 
