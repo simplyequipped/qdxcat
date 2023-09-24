cd49638f072c5c2a340d329ea74b2c446747629e
OtisByron <howard@simplyequipped.com>
2023-09-24 16:45:09 -0400

    consistent format and structure

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
 setup(
     name='qdxcat',
-    version='0.2',
+    version='0.2.0',
     author='Simply Equipped LLC',
     author_email='howard@simplyequipped.com',
     description='Serial CAT control for QRPLabs QDX transceiver',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/simplyequipped/qdxcat',
     packages=setuptools.find_packages(),
     install_requires=['pyserial'],
     classifiers=[
@@ -14,6 +20,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows'
-    ]
+    ],
+    python_requires='>=3.6'
 )
 
