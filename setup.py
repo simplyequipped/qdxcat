e816a9b14093d94c7f04172c8af526ac1bedbcbd
OtisByron <howard@simplyequipped.com>
2023-10-06 10:48:34 -0400

    update setup import

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+import setuptools
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='qdxcat',
     version='0.2.0',
     author='Simply Equipped LLC',
